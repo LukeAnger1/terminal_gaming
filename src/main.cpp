db75b51610983a333784a130e606b5f15796de00
Luke <anger.luke2004@gmail.com>
2026-09-18 12:28:06 -0400

    Refactor: Removed dead code and moved program control

--- src/main.cpp
+++ src/main.cpp
@@ -9,30 +9,7 @@
 #include <stdio.h>
 #include <iostream>
 
-struct termios orig_termios;
-
-void die(const char *s) {
-  perror(s);
-  exit(1);
-}
-
-void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-    die("tcsetattr");
-}
-
-void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
-  atexit(disableRawMode);
-  struct termios raw = orig_termios;
-  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-  raw.c_oflag &= ~(OPOST);
-  raw.c_cflag |= (CS8);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 1;
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
-}
+#include "utils/program_control.cpp"
 
 int main() {
   enableRawMode();
@@ -46,7 +23,7 @@ int main() {
 
   while (1) {
     char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) endProgram("read");
     read(STDIN_FILENO, &c, 1);
     if (iscntrl(c)) {
       printf("%d\r\n", c);
@@ -58,39 +35,3 @@ int main() {
 
   return 0;
 }
-
-// #include <SFML/Graphics.hpp>
-
-// int main()
-// {
-//     sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-//     sf::CircleShape shape(100.f);
-//     shape.setFillColor(sf::Color::Green);
-
-//     while (window.isOpen())
-//     {
-//         while (const std::optional event = window.pollEvent())
-//         {
-//             if (event->is<sf::Event::Closed>())
-//                 window.close();
-//         }
-
-//         window.clear();
-//         window.draw(shape);
-//         window.display();
-//     }
-// }
-
-// #include "user_input/BufferToggle.hpp"
-
-// int main() {
-
-//     // BEGIN CREDIT: https://cplusplus.com/forum/general/29137/
-//     BufferToggle bt;
-//     std::getchar(); //waits for you to press enter before proceeding to the next instruction
-//     bt.off();
-//     std::getchar(); //processes next instruction as soon as you type a character (no enter)
-//     bt.on();
-//     std::getchar(); //waits for you to press enter before proceeding to the next instructionhttps://cplusplus.com/forum/general/29137/
-//     // END CREDIT
-// }
---
+++ src/utils/program_control.cpp
@@ -0,0 +1,35 @@
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <iostream>
+
+void endProgram(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+// Sets up the user input to listen from terminal clicking
+// CREDIT: https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
+//   Sets up raw mode for terminal input and gameplay
+struct termios orig_termios;
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    endProgram("tcsetattr");
+}
+
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) endProgram("tcgetattr");
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) endProgram("tcsetattr");
+}
