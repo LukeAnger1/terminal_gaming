bed4579938d1d5e121066c9e218c3beb9466ebdc
Luke <anger.luke2004@gmail.com>
2026-09-18 12:13:07 -0400

    Feat: Added debug macro

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <iostream>
 
 struct termios orig_termios;
 
@@ -35,6 +36,13 @@ void enableRawMode() {
 
 int main() {
   enableRawMode();
+
+  // Notify the user if in debug mode
+  #if DEBUG==1
+    std::cout << "In debug mode\n";
+  #else
+    std::cout << "Not in debug mode\n";
+  #endif
 
   while (1) {
     char c = '\0';
